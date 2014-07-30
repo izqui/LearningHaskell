04fc18e9e1f8df7de973ce5a8a98ab68440a5580
Jorge Izquierdo <jorge@izqui.me>
2014-07-30 17:26:24 -0400

    Fix local max

--- day3/localmaxima.hs
+++ day3/localmaxima.hs
@@ -3,7 +3,7 @@ data List = N List Int List
 
 localmax :: [Integer] -> [Integer]
 localmax (x:y:z:xs)
-	| max y (max x z) == y = y:localmax xs
+	| max y (max x z) == y = y:localmax (z:xs)
 	| otherwise = localmax $ [y,z]++xs
 localmax [_, _] = []
 localmax _ = []
