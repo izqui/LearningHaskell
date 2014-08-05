77ca115862b23f68334d63050fa32f474d0b5233
Jorge Izquierdo <jorge@izqui.me>
2014-08-05 16:34:53 -0400

    Calc with more types

--- day5/Calc.hs
+++ day5/Calc.hs
@@ -23,7 +23,7 @@ instance Num ExprT where
 	signum (Lit i) = fromInteger $ signum i
 	signum _ = fromInteger 1
 
-	fromInteger i = Lit i
+	fromInteger = Lit
 
 eval' :: ExprT -> Integer
 eval' (Lit i) = i
@@ -34,8 +34,57 @@ evalStr :: String -> Maybe Integer
 evalStr str 
 	| res == Nothing = Nothing
 	| otherwise = Just $ eval' $ unwrap res
-	where res = parseExp Lit Add Mul str
+	where res = parseExp lit add mul str
 
 unwrap :: Maybe a -> a
 unwrap Nothing = error "Unwraping nothing"
 unwrap (Just x) = x
+
+class Expr a where
+	lit :: Integer -> a
+	mul :: a -> a -> a
+	add :: a -> a -> a
+
+instance Expr ExprT where
+	lit = Lit
+	add = Add 
+	mul = Mul
+
+instance Expr Integer where
+	lit = id
+	add = (+)
+	mul = (*)
+
+instance Expr Bool where
+	lit i = i > 0
+	add = (||)
+	mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+instance Ord MinMax where
+	(<=) (MinMax i) (MinMax j) = i<=j
+
+instance Expr MinMax where
+	lit = MinMax
+	add = max 
+	mul = min
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+instance Num Mod7 where
+	(+) (Mod7 i) (Mod7 j) = fromInteger $ i+j
+	(*) (Mod7 i) (Mod7 j) = fromInteger $ i*j
+
+	abs (Mod7 i) = fromInteger $ abs i
+	signum (Mod7 i) = fromInteger $ signum i
+
+	fromInteger i 
+		| i < 7 = Mod7 i
+		| otherwise = Mod7 $ mod i 7  
+
+instance Expr Mod7 where
+	lit = Mod7
+	add = (+)
+	mul = (*)
+
+testExp :: Expr a => String -> Maybe a
+testExp = parseExp lit add mul
